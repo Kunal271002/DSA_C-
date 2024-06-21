1a1f2b1bd2716881be58b0ed16230e917b114c50
Kunal Sharma <kk.s67533@gmail.com>
2024-06-21 23:30:56 +0530

    Faizan

--- MaximumOrMinimum.cpp
+++ MaximumOrMinimum.cpp
@@ -1,7 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr;
+    int n = arr.size()
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i = 0;i<n;i++){
+        cout<<arr[i];
+    }
+    return 0 ;
+}
+
+
 // #include<iostream>
 // using namespace std;
 // int main(){
-//     int n,max;
+//     int n,min;
 //     cout<<"Enter the size of Array : ";
 //     cin>>n;
 //     int Array[n];
@@ -11,41 +27,15 @@
 //     for(int i = 0;i<n;i++){
 //         cout<<Array[i]<<" ";
 //     }
-//     cout<<"\nTo find the Maximum of All the Value"<<endl;
+//     cout<<"\nTo find the Minimum of All the Value"<<endl;
 //     for(int i = 0;i<n;i++){
-//        for(int j = 1;j<n;j++){
+//        for(int j = 0;j<n;j++){
 //         if(Array[j]>Array[i]){
-//             max = Array[j];
+//             min = Array[i];
 //         }
 //        }
     
-//     }cout<<max;
+//     }cout<<min;
 
 // }
-
-
-#include<iostream>
-using namespace std;
-int main(){
-    int n,min;
-    cout<<"Enter the size of Array : ";
-    cin>>n;
-    int Array[n];
-    for(int i = 0;i<n;i++){
-        cin>>Array[i];
-    }
-    for(int i = 0;i<n;i++){
-        cout<<Array[i]<<" ";
-    }
-    cout<<"\nTo find the Minimum of All the Value"<<endl;
-    for(int i = 0;i<n;i++){
-       for(int j = 0;j<n;j++){
-        if(Array[j]>Array[i]){
-            min = Array[i];
-        }
-       }
-    
-    }cout<<min;
-
-}
 
